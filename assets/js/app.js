2dd0d1bd4ebd98726d540c97d26178789609c3ab
Carlos Cruz Valencia <a2carcruzinfo@gmail.com>
2023-05-26 22:26:57 +0200

    rediseño y optimisacion del codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,29 @@
-function newColor(){
-    letter = ['A', 'B', 'C', 'D', 'E', 'F']
-    color = []
-    for (i = 0; i != 3; i++) {
+function newColor() {
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+  
+    for (let i = 0; i < 5; i++) {
+        let finalColor = '';
 
-        number = Math.random() * 9;
-        number2 = Math.random() * 5;
-        trunc = Math.trunc(number)
-        trunc2 = Math.trunc(number2)
+        for (let j = 0; j < 3; j++) {
+            const number = Math.trunc(Math.random() * 10);
+            const letterIndex = Math.trunc(Math.random() * 6);
+            finalColor += number + letters[letterIndex];
+        }
 
+        const cardColor = document.getElementById("pantoneColor" + i);
+        cardColor.style.backgroundColor = "#" + finalColor;
+        
+        const cardHex = document.getElementById("pantoneColorHex" + i);
+        cardHex.innerHTML = "#" + finalColor;
 
-        color.push(trunc)
-        color.push(letter[trunc2])
+        const n_match = ntc.name("#" + finalColor);
+        const n_name = n_match[1];
+
+        const cardName = document.getElementById("pantoneColorName" + i);
+        cardName.innerHTML = n_name;
     }
-
-    finalColor = color.join('')
-    
-    cardColor = document.getElementById("pantoneColor");
-    cardColor.style.backgroundColor = "#" + finalColor;
-    cardHex = document.getElementById("pantoneColorHex")
-    cardHex.innerHTML = "#"+finalColor;
-    
-
-
-
-    var n_match  = ntc.name("#"+finalColor);
-    n_name       = n_match[1]; // This is the text string for the name of the match
-
-    // alert(n_match);
-    cardName = document.getElementById("pantoneColorName")
-    cardName.innerHTML = n_name;
-
 }
-function reload(){
+function reload() {
     document.getElementById("btImage").style.transform = "rotate(360deg)"
     document.getElementById("btImage").style.transition = "0.5s"
     setTimeout(() => {
